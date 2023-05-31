4f771d024e742262b4efb16ec9d09b3617d48830
Benna96 <vissersofia@gmail.com>
2023-05-31 13:14:43 +0300

    Change Player input to use Send Messages, simpler

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(PlayerInput))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Rigidbody rigidBody;
@@ -13,11 +14,13 @@ public class PlayerMovement : MonoBehaviour
         rigidBody.velocity = movementSpeed * ((transform.forward * movementInput.y) + (transform.right * movementInput.x));
     }
 
-    /// <summary>
-    /// Called by Player Input component when movement direciton changes
-    /// </summary>
-    public void UpdateMovementDirection(InputAction.CallbackContext context)
+    public void OnMove(InputValue value)
     {
-        movementInput = context.ReadValue<Vector2>();
+        movementInput = value.Get<Vector2>();
+    }
+
+    public void OnCrouch()
+    {
+        Debug.Log("Pressed crouch button");
     }
 }
