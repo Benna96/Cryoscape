edcf9ce9344d816c1dc6e8428c3b000dc4510d03
Jeesuz <juha.hietala53@gmail.com>
2023-06-01 15:10:18 +0300

    spacesuit pod script better

--- Assets/Scripts/SpacesuitPod.cs
+++ Assets/Scripts/SpacesuitPod.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class SpacesuitPod : MonoBehaviour
 {
+    GameObject padBackground;
     ButtonScript buttonTwo, buttonSeven, buttonEight, buttonNine, buttonTen, buttonEleven, buttonThirteen, buttonSixteen;
 
     // Start is called before the first frame update
     void Start()
     {
-        buttonTwo = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(1).GetComponent<ButtonScript>();
-        buttonSeven = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(6).GetComponent<ButtonScript>();
-        buttonEight = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(7).GetComponent<ButtonScript>();
-        buttonNine = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(8).GetComponent<ButtonScript>();
-        buttonTen = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(9).GetComponent<ButtonScript>();
-        buttonEleven = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(10).GetComponent<ButtonScript>();
-        buttonThirteen = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(12).GetComponent<ButtonScript>();
-        buttonSixteen = GameObject.Find("SpacesuitPad").transform.GetChild(0).GetChild(15).GetComponent<ButtonScript>();
+        padBackground = GameObject.Find("SpacesuitPad").transform.GetChild(0).gameObject;
+
+        buttonTwo = padBackground.transform.GetChild(1).GetComponent<ButtonScript>();
+        buttonSeven = padBackground.transform.GetChild(6).GetComponent<ButtonScript>();
+        buttonEight = padBackground.transform.GetChild(7).GetComponent<ButtonScript>();
+        buttonNine = padBackground.transform.GetChild(8).GetComponent<ButtonScript>();
+        buttonTen = padBackground.transform.GetChild(9).GetComponent<ButtonScript>();
+        buttonEleven = padBackground.transform.GetChild(10).GetComponent<ButtonScript>();
+        buttonThirteen = padBackground.transform.GetChild(12).GetComponent<ButtonScript>();
+        buttonSixteen = padBackground.transform.GetChild(15).GetComponent<ButtonScript>();
 
         Debug.Log(buttonTwo.name);
         Debug.Log(buttonSeven.name);
@@ -31,8 +34,7 @@ public class SpacesuitPod : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (
-            buttonTwo.interacted && buttonSeven.interacted && buttonEight.interacted && buttonNine.interacted &&
+        if (buttonTwo.interacted && buttonSeven.interacted && buttonEight.interacted && buttonNine.interacted &&
             buttonTen.interacted && buttonEleven.interacted && buttonThirteen.interacted && buttonSixteen.interacted)
             {
                 Debug.Log("Spacesuit Pod is open.");
