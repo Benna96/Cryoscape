28d65fe9e47298aff8b420f9a12b9e4e406c66dd
Jeesuz <juha.hietala53@gmail.com>
2023-06-02 15:08:23 +0300

    button pad working

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -6,11 +6,13 @@ public class ButtonScript : Interactable
 {
     public bool interacted;
     public string buttonName;
+    OpenAnim buttonPress;
     // Start is called before the first frame update
     void Start()
     {
         interacted = false;
         buttonName = gameObject.name;
+        buttonPress = transform.GetChild(0).GetComponent<OpenAnim>();
     }
 
     // Update is called once per frame
@@ -24,10 +26,12 @@ public class ButtonScript : Interactable
         if (interacted)
         {
             interacted = false;
+            StartCoroutine(buttonPress.Close());
             Debug.Log($"{name} unpressed");
         } else if (!interacted)
         {
             interacted = true;
+            StartCoroutine(buttonPress.Open());
             Debug.Log($"{name} pressed");
         }
     }
