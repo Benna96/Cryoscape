da9d8da9ac53f1c5a40d57f3dc46ddc568416d0f
Benna96 <vissersofia@gmail.com>
2023-06-02 12:38:26 +0300

    Refactor OpenAnimTurning's axis into a helper & make it use local rotation

--- Assets/Scripts/OpenAnimTurning.cs
+++ Assets/Scripts/OpenAnimTurning.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenAnimTurning : OpenAnim
 {
-    public enum Axis
-    {
-        X,
-        Y,
-        Z
-    }
 
     [Header("Turning specific")]
-    [SerializeField] private Axis axis;
+    [SerializeField] private AxisHelper.Axis axis;
     [SerializeField] private float angle;
 
     private Vector3 axisVector;
@@ -21,18 +13,12 @@ public class OpenAnimTurning : OpenAnim
 
     private void Awake()
     {
-        axisVector = axis switch
-        {
-            Axis.X => Vector3.right,
-            Axis.Y => Vector3.up,
-            Axis.Z => Vector3.forward,
-            _ => Vector3.zero
-        };
+        axisVector = axis.AsVector3();
 
-        origRotation = transform.rotation;
+        origRotation = transform.localRotation;
         targetRotation = origRotation * Quaternion.AngleAxis(angle, axisVector);
     }
 
     protected override void AnimateAtPoint(float evaluatedPoint)
-        => transform.rotation = Quaternion.LerpUnclamped(origRotation, targetRotation, evaluatedPoint);
+        => transform.localRotation = Quaternion.LerpUnclamped(origRotation, targetRotation, evaluatedPoint);
 }
