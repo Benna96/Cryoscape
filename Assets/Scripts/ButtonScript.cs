53bb72703bc6666e28e770dc0e91d3c7825364b1
Benna96 <vissersofia@gmail.com>
2023-06-03 13:41:51 +0300

    Trigger related animations after button press finishes

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+
 using UnityEngine;
 
 public class ButtonScript : Interactable
 {
+    [SerializeField] private SimpleAnim[] pressTriggeredAnims;
+
     public bool interacted;
     public string buttonName;
     SimpleAnim[] buttonPressAnims;
+    private float buttonPressDuration;
     // Start is called before the first frame update
     void Start()
     {
         interacted = false;
         buttonName = gameObject.name;
         buttonPressAnims = transform.GetChild(0).GetComponents<SimpleAnim>();
+        buttonPressDuration = buttonPressAnims.Select(x => x.AnimDuration).Max();
     }
 
     // Update is called once per frame
@@ -26,13 +33,26 @@ public class ButtonScript : Interactable
         if (interacted)
         {
             interacted = false;
-            Array.ForEach(buttonPressAnims, anim => StartCoroutine(anim.AnimateNormal()));
-            Debug.Log($"{name} unpressed");
+            StartCoroutine(Unpress());
         } else if (!interacted)
         {
             interacted = true;
-            Array.ForEach(buttonPressAnims, anim => StartCoroutine(anim.AnimateReversed()));
-            Debug.Log($"{name} pressed");
+            StartCoroutine(Press());
         }
+    }
+
+    private IEnumerator Press()
+    {
+        Array.ForEach(buttonPressAnims, anim => StartCoroutine(anim.AnimateNormal()));
+        yield return new WaitForSeconds(buttonPressDuration);
+
+        Array.ForEach(pressTriggeredAnims, anim => StartCoroutine(anim.AnimateNormal()));
+    }
+
+    private IEnumerator Unpress()
+    {
+        Array.ForEach(buttonPressAnims, anim => StartCoroutine(anim.AnimateReversed()));
+        Array.ForEach(pressTriggeredAnims, anim => StartCoroutine(anim.AnimateReversed()));
+        yield return new WaitForSeconds(buttonPressDuration);
     }
 }
