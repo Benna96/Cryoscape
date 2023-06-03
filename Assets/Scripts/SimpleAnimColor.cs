7d54dcdadc9fbd1b726ae3c50a6ccc1c0d93ed5b
Benna96 <vissersofia@gmail.com>
2023-06-03 13:31:35 +0300

    Add color anim

---
+++ Assets/Scripts/SimpleAnimColor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SimpleAnimColor : SimpleAnim
+{
+    [Tooltip("Mesh will be set to this color on awake")]
+    [SerializeField] private Color startColor = Color.white;
+
+    [SerializeField] private Color endColor = Color.white;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.color = startColor;
+    }
+
+    protected override void AnimateAtPoint(float evaluatedPoint)
+        => meshRenderer.material.color = Color.LerpUnclamped(startColor, endColor, evaluatedPoint);
+}
