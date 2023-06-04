f9cb9f504c3ad1fadb85b7c7a486a430143ab9b8
Benna96 <vissersofia@gmail.com>
2023-06-04 18:58:06 +0300

    Disable domes on spacesuit pickup

---
+++ Assets/Scripts/DisableOnItemAcquired.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Specialized;
+
+using UnityEngine;
+
+public class DisableOnItemAcquired : MonoBehaviour
+{
+    [SerializeField] private Item itemToDisableOn;
+
+    private void Start()
+    {
+        (InventoryManager.instance.items as INotifyCollectionChanged).CollectionChanged += InventoryItemsChanged;
+    }
+
+    private void InventoryItemsChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add)
+            foreach (InventoryItem newItem in e.NewItems)
+                if (newItem.item.id == itemToDisableOn.id)
+                {
+                    (InventoryManager.instance.items as INotifyCollectionChanged).CollectionChanged -= InventoryItemsChanged;
+                    gameObject.SetActive(false);
+                    break;
+                }
+    }
+}
