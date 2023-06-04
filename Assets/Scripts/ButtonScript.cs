36c7424629535a7146f042c8a10db06be40f09f0
Benna96 <vissersofia@gmail.com>
2023-06-04 16:09:47 +0300

    Change pressTriggeredAnims to activateCompletedAnims

--- Assets/Scripts/ButtonScript.cs
+++ Assets/Scripts/ButtonScript.cs
@@ -7,8 +7,6 @@ using UnityEngine;
 
 public class ButtonScript : Activatable
 {
-    [SerializeField] private SimpleAnim[] pressTriggeredAnims;
-
     public string buttonName;
     // Start is called before the first frame update
     void Start()
