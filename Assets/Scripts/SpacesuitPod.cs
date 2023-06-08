faa9476dd320b66a9f7bdac9e291a99f777e5d58
Jeesuz <juha.hietala53@gmail.com>
2023-06-08 10:27:16 +0300

    removed redundant button script

--- Assets/Scripts/SpacesuitPod.cs
+++ Assets/Scripts/SpacesuitPod.cs
@@ -6,7 +6,7 @@ public class SpacesuitPod : MonoBehaviour
 {
     SimpleAnim podOpening;
     GameObject padBackground;
-    ButtonScript buttonTwo, buttonSeven, buttonEight, buttonNine, buttonTen, buttonEleven, buttonThirteen, buttonSixteen,
+    Activatable buttonTwo, buttonSeven, buttonEight, buttonNine, buttonTen, buttonEleven, buttonThirteen, buttonSixteen,
             buttonOne, buttonThree, buttonFour, buttonFive, buttonSix, buttonTwelve, buttonFourteen, buttonFifteen;
     bool podIsOpen = false;
 
@@ -16,22 +16,22 @@ public class SpacesuitPod : MonoBehaviour
         podOpening = gameObject.transform.GetChild(0).gameObject.GetComponent<SimpleAnim>();
         padBackground = GameObject.Find("SpacesuitPad").transform.GetChild(0).gameObject;
 
-        buttonTwo = padBackground.transform.GetChild(1).GetComponent<ButtonScript>();
-        buttonSeven = padBackground.transform.GetChild(6).GetComponent<ButtonScript>();
-        buttonEight = padBackground.transform.GetChild(7).GetComponent<ButtonScript>();
-        buttonNine = padBackground.transform.GetChild(8).GetComponent<ButtonScript>();
-        buttonTen = padBackground.transform.GetChild(9).GetComponent<ButtonScript>();
-        buttonEleven = padBackground.transform.GetChild(10).GetComponent<ButtonScript>();
-        buttonThirteen = padBackground.transform.GetChild(12).GetComponent<ButtonScript>();
-        buttonSixteen = padBackground.transform.GetChild(15).GetComponent<ButtonScript>();
-        buttonOne = padBackground.transform.GetChild(0).GetComponent<ButtonScript>();
-        buttonThree = padBackground.transform.GetChild(2).GetComponent<ButtonScript>();
-        buttonFour = padBackground.transform.GetChild(3).GetComponent<ButtonScript>();
-        buttonFive = padBackground.transform.GetChild(4).GetComponent<ButtonScript>();
-        buttonSix = padBackground.transform.GetChild(5).GetComponent<ButtonScript>();
-        buttonTwelve = padBackground.transform.GetChild(11).GetComponent<ButtonScript>();
-        buttonFourteen = padBackground.transform.GetChild(13).GetComponent<ButtonScript>();
-        buttonFifteen = padBackground.transform.GetChild(14).GetComponent<ButtonScript>();
+        buttonTwo = padBackground.transform.GetChild(1).GetComponent<Activatable>();
+        buttonSeven = padBackground.transform.GetChild(6).GetComponent<Activatable>();
+        buttonEight = padBackground.transform.GetChild(7).GetComponent<Activatable>();
+        buttonNine = padBackground.transform.GetChild(8).GetComponent<Activatable>();
+        buttonTen = padBackground.transform.GetChild(9).GetComponent<Activatable>();
+        buttonEleven = padBackground.transform.GetChild(10).GetComponent<Activatable>();
+        buttonThirteen = padBackground.transform.GetChild(12).GetComponent<Activatable>();
+        buttonSixteen = padBackground.transform.GetChild(15).GetComponent<Activatable>();
+        buttonOne = padBackground.transform.GetChild(0).GetComponent<Activatable>();
+        buttonThree = padBackground.transform.GetChild(2).GetComponent<Activatable>();
+        buttonFour = padBackground.transform.GetChild(3).GetComponent<Activatable>();
+        buttonFive = padBackground.transform.GetChild(4).GetComponent<Activatable>();
+        buttonSix = padBackground.transform.GetChild(5).GetComponent<Activatable>();
+        buttonTwelve = padBackground.transform.GetChild(11).GetComponent<Activatable>();
+        buttonFourteen = padBackground.transform.GetChild(13).GetComponent<Activatable>();
+        buttonFifteen = padBackground.transform.GetChild(14).GetComponent<Activatable>();
     }
 
     // Update is called once per frame
