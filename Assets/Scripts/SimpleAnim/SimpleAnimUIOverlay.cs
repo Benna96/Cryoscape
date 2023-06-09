8b7b5319ff8f418e9a50f7c50cf4408eba1a522e
Benna96 <vissersofia@gmail.com>
2023-06-09 19:47:01 +0300

    Add SimpleAnimUIOverlay & add frost effect to keycard. Resolves #94

---
+++ Assets/Scripts/SimpleAnim/SimpleAnimUIOverlay.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class SimpleAnimUIOverlay : SimpleAnim
+{
+    [SerializeField] public EffectOverlaysUI.Overlay overlay;
+
+    protected override void AnimateAtPoint(float evaluatedPoint)
+    {
+        UIManager.instance.SetOverlayEffectOpacity(overlay, evaluatedPoint);
+    }
+}
