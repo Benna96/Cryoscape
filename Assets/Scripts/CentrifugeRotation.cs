5ca86904a9a81eff95bfe44a6e6d654a8b22d902
ankeahko <hannes.hokkanen@gmail.com>
2023-06-13 01:07:39 +0300

    CleanUp

--- Assets/Scripts/CentrifugeRotation.cs
+++ Assets/Scripts/CentrifugeRotation.cs
@@ -2,11 +2,26 @@ using UnityEngine;
 
 public class CentrifugeRotation : MonoBehaviour
 {
-    public float rotationSpeed = 100.0f;
+    public float rotationSpeed = 1.0f;
+    public float wobbleAmount = 0.08f;
+    public float wobbleSpeed = 0.2f;
+
+    private float originalY;
+
+    private void Start()
+    {
+        originalY = transform.position.y;
+    }
 
     private void Update()
     {
-        // Rotate the object around the X-axis
-        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime);
+        // Rotate the object around the Y-axis
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        // Apply wobbling effect
+        float wobbleOffset = Mathf.Sin(Time.time * wobbleSpeed) * wobbleAmount;
+        Vector3 newPosition = transform.position;
+        newPosition.y = originalY + wobbleOffset;
+        transform.position = newPosition;
     }
 }
