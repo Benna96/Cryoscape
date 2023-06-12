c85bad7582d24cd5632be0bac00f58c37415b257
Benna96 <vissersofia@gmail.com>
2023-06-12 10:53:30 +0300

    SimpleAnimColor changes

--- Assets/Scripts/SimpleAnim/SimpleAnimColor.cs
+++ Assets/Scripts/SimpleAnim/SimpleAnimColor.cs
@@ -4,17 +4,19 @@
 public class SimpleAnimColor : SimpleAnim
 {
     [Tooltip("Mesh will be set to this color on awake")]
-    [SerializeField] private Color startColor = Color.white;
+    [SerializeField] public Color startColor = Color.white;
 
-    [SerializeField] private Color endColor = Color.white;
+    [SerializeField] public Color endColor = Color.white;
 
     private MeshRenderer meshRenderer;
 
     private void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
-        meshRenderer.material.color = startColor;
+        SyncColor();
     }
+
+    public void SyncColor() => meshRenderer.material.color = startColor;
 
     protected override void AnimateAtPoint(float evaluatedPoint)
         => meshRenderer.material.color = Color.LerpUnclamped(startColor, endColor, evaluatedPoint);
