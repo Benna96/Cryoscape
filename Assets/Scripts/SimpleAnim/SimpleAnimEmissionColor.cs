c752e62cf971e3650d4d54c3d269804d9f64a23b
Jeesuz <juha.hietala53@gmail.com>
2023-06-21 14:37:01 +0300

    simple anim for emission material

---
+++ Assets/Scripts/SimpleAnim/SimpleAnimEmissionColor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class SimpleAnimEmissionColor : SimpleAnim
+{
+    [Tooltip("Mesh will be set to this color on awake")]
+    [SerializeField] public Color startColor = Color.white;
+
+    [SerializeField] public Color endColor = Color.white;
+
+    private MeshRenderer meshRenderer;
+
+    private void Awake()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material.EnableKeyword("_EMISSION");
+        SyncEmissionColor();
+    }
+
+    public void SyncEmissionColor() => meshRenderer.material.SetColor("_EmissionColor", startColor);
+
+    protected override void AnimateAtPoint(float evaluatedPoint)
+        => meshRenderer.material.SetColor("_EmissionColor", Color.LerpUnclamped(startColor, endColor, evaluatedPoint));
+}
