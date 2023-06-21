0fab42f6ae05ee41d74924b9e819c04418026e5f
Jeesuz <juha.hietala53@gmail.com>
2023-06-21 13:42:16 +0300

    added code for light color changing

---
+++ Assets/Scripts/SimpleAnim/SimpleAnimLight.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+public class SimpleAnimLight : SimpleAnim
+{
+    [Tooltip("Light will be set to this color on awake")]
+    [SerializeField] public Color startColor = Color.white;
+
+    [SerializeField] public Color endColor = Color.white;
+
+    private Light lightComponent;
+
+    private void Awake()
+    {
+        lightComponent = GetComponent<Light>();
+        SyncLight();
+    }
+
+    public void SyncLight() => lightComponent.color = startColor;
+
+    protected override void AnimateAtPoint(float evaluatedPoint)
+        => lightComponent.color = Color.LerpUnclamped(startColor, endColor, evaluatedPoint);
+}
